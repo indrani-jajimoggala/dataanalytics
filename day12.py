d010bc181e0680294b642b392aab5d51e0814f1a
indrani-jajimoggala <jajimoggalaindrani58@gmail.com>
2026-05-06 12:29:22 +0530

    for loop

---
+++ day12.py
@@ -0,0 +1,20 @@
+'''
+so = list(input("enter item of list: "))
+result = []
+for i in so:
+    if i not in result:
+        result.append(i)
+print(result)
+'''
+'''
+nums = [10,2,20,76,4,45,99]
+max1 = 0
+max2 = 0
+
+for num in nums:
+    if num>max1:
+        max2=max1
+        max1=num
+print(f"{max2} is the max number in the {nums}")
+'''
+
